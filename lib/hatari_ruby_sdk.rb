134702723edfb87c3129222cec0824f9bd216aea
Vicky Djoulako <vicky@voyancehq.com>
2022-12-12 06:25:42 +0100

    initial commit

---
+++ lib/hatari_ruby_sdk.rb
@@ -0,0 +1,6 @@
+require "hatari_ruby_sdk/version"
+
+module HatariRubySdk
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/hatari_ruby_sdk/version.rb
@@ -0,0 +1,3 @@
+module HatariRubySdk
+  VERSION = "0.1.0"
+end
